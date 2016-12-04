23b0fe33c06dc97c22ae72d2fe313f8310196259
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2016-12-04 18:21:39 +0300

    registering and unregistering filesystem

--- hello.c
+++ hello.c
@@ -1,18 +1,40 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/fs.h>
 
-static int hello_init(void){
-	printk(KERN_ALERT "Kernel module have been initialized\n");
+
+static struct dentry *_mount(struct file_system_type *fs_type,
+	int flags, const char *dev_name, void *data) {
+
+	struct dentry *ret;
+	printk(KERN_ALERT "Mounting filesystem\n");
+	ret = NULL;
+	return ret; 
+}
+
+struct file_system_type efs = {
+ 	.owner = THIS_MODULE,
+	.name = "efs",
+	.mount = _mount,
+};
+
+
+static int efs_init(void){
+	int ret;
+	ret = register_filesystem(&efs);
+	printk(KERN_ALERT "Registering filesystem name\n");
 	return 0;
 }
 
 
-static void hello_exit(void){
-	printk(KERN_ALERT "Exit\n");
+static void efs_exit(void){
+	int ret;
+	ret = unregister_filesystem(&efs);
+	printk(KERN_ALERT "Uninstalling filesystem\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(efs_init);
+module_exit(efs_exit);
 
 
 MODULE_LICENSE("GPL");
