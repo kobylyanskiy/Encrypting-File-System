6d2c2f1745ac48afa3566a0e28519e759bc62e4e
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2017-01-10 14:27:35 +0300

    added inode of root dentry support

--- mkfs.c
+++ mkfs.c
@@ -1,38 +1,42 @@
-#include "module.h" 
- 
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/types.h> 
 #include <sys/stat.h> 
+#include <stdint.h>
 #include <fcntl.h> 
- 
-int main(int argc, char *argv[]) 
-{ 
-  int fd; 
-  ssize_t ret; 
-  struct efs_super_block sb; 
- 
-  if (argc != 2) { 
-    printf("Usage: mkfs <device>\n"); 
-    return -1; 
-  } 
- 
-  fd = open(argv[1], O_RDWR); 
-  if (fd == -1) { 
-    perror("Error opening the device"); 
-    return -1; 
-  } 
- 
-  sb.version = 1; 
-  sb.magic = EFS_MAGIC_NUMBER; 
-  sb.block_size = EFS_DEFAULT_BLOCK_SIZE; 
-  sb.free_blocks = ~0; 
- 
-  ret = write(fd, (char *)&sb, sizeof(sb)); 
- 
-  printf("Super block written succesfully"); 
- 
-  close(fd); 
- 
-  return 0; 
+#include "module.h" 
+
+int main(int argc, char *argv[]) { 
+	int fd; 
+	ssize_t ret; 
+	struct efs_super_block sb; 
+	
+	if (argc != 2) { 
+	  printf("Usage: mkfs <device>\n"); 
+	  return -1; 
+	} 
+	
+	fd = open(argv[1], O_RDWR); 
+	if (fd == -1) { 
+	  perror("Error opening the device"); 
+	  return -1; 
+	} 
+	
+	sb.version = 1; 
+	sb.magic = EFS_MAGIC_NUMBER; 
+	sb.block_size = EFS_DEFAULT_BLOCK_SIZE; 
+	sb.free_blocks = ~0; 
+	
+	sb.root_inode.mode = S_IFDIR; 
+	sb.root_inode.inode_no = EFS_ROOT_INODE_NUMBER; 
+	sb.root_inode.data_block_number = EFS_ROOTDIR_DATABLOCK_NUMBER; 
+	sb.root_inode.dir_children_count = 0;
+	
+	ret = write(fd, (char *)&sb, sizeof(sb)); 
+	
+	printf("Super block written succesfully, %ld bytes was written", ret); 
+	
+	close(fd); 
+	
+	return 0; 
 } 
--- module.h
+++ module.h
@@ -1,12 +1,32 @@
-static unsigned long const EFS_MAGIC_NUMBER = 0x1488228;
+const int EFS_DEFAULT_BLOCK_SIZE = 4096; 
+#define EFS_MAGIC_NUMBER 0x3425887 
+#define EFS_DEFAULT_BLOCK_SIZE 4096 
+#define EFS_FILENAME_MAXLEN 255 
+ 
+const int EFS_ROOT_INODE_NUMBER = 1; 
+const int EFS_ROOTDIR_DATABLOCK_NUMBER = 2;
 
-const int EFS_DEFAULT_BLOCK_SIZE = 4 * 1024; 
+struct efs_dir_record { 
+  char filename[EFS_FILENAME_MAXLEN]; 
+  uint32_t inode_no; 
+}; 
  
+struct efs_inode { 
+  mode_t mode; 
+  uint32_t inode_no; 
+  uint32_t data_block_number; 
+ 
+  union { 
+    uint32_t file_size; 
+    uint32_t dir_children_count; 
+  }; 
+}; 
+
 struct efs_super_block { 
-  unsigned int version; 
-  unsigned int magic; 
-  unsigned int block_size; 
-  unsigned int free_blocks; 
- 
-  char padding[ (4 * 1024) - (4 * sizeof(unsigned int))]; 
+	uint32_t version; 
+	uint32_t magic; 
+	uint32_t block_size; 
+	uint32_t free_blocks;
+	struct efs_inode root_inode;
+	char padding[(4 * 1024) - (4 * sizeof(uint32_t))]; 
 };
